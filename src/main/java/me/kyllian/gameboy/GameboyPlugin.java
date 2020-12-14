1e2b784aa2747f78b2e1e3069ecbad471cd968f1
InstantlyMoist <kyllian007@gmail.com>
2020-12-14 12:17:37 +0100

    Added new chart to bStats

--- src/main/java/me/kyllian/gameboy/GameboyPlugin.java
+++ src/main/java/me/kyllian/gameboy/GameboyPlugin.java
@@ -11,7 +11,10 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 public class GameboyPlugin extends JavaPlugin {
 
@@ -34,6 +37,14 @@ public class GameboyPlugin extends JavaPlugin {
 
         Metrics metrics = new Metrics(this, 9592);
         metrics.addCustomChart(new Metrics.SingleLineChart("games_emulated", () -> gamesEmulated));
+        metrics.addCustomChart(new Metrics.AdvancedPie("games_installed", () -> {
+            Map<String, Integer> values = new HashMap<>();
+            romHandler.getRoms().keySet().forEach(romName -> {
+                values.put(romName, 1);
+            });
+            return values;
+        }));
+
 
         mapHandler.loadData();
 
